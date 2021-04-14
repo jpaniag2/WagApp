e54bc74ad570e0e4426c39d9808284b400c9dad5
Andrea <aperalt3@uncc.edu>
2021-04-13 23:43:48 -0400

    Updated register button sequence to profile page.

--- app/src/main/java/com/group11/wagapp/SignUpPage.kt
+++ app/src/main/java/com/group11/wagapp/SignUpPage.kt
@@ -3,17 +3,16 @@ package com.group11.wagapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up_page.*
 
-class Login : AppCompatActivity() {
+class SignUpPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up_page)
 
-        loginButton.setOnClickListener {
-            val intent = Intent(this, SignUpPage::class.java)
+        registerUserButton.setOnClickListener {
+            val intent = Intent(this, UserHomeProfile::class.java)
             startActivity(intent)
         }
-
     }
 }
