a68908cd66b502a1c2d55a24feff395f3118fee8
Julio Paniagua <jpaniag2@gmail.com>
2021-04-13 20:01:29 -0400

    Created User Home Profile.

---
+++ app/src/main/java/com/group11/wagapp/UserHomeProfile.kt
@@ -0,0 +1,11 @@
+package com.group11.wagapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserHomeProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_home_profile)
+    }
+}
