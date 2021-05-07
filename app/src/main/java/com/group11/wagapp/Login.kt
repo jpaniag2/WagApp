93f448b1959a3abb2c4a06c1f4d358549e15464d
aperalt3 <aperalt3@uncc.edu>
2021-05-06 21:10:14 -0400

    Updated login and sign up requirements.

--- app/src/main/java/com/group11/wagapp/Login.kt
+++ app/src/main/java/com/group11/wagapp/Login.kt
@@ -1,24 +1,37 @@
+
 package com.group11.wagapp
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
+
         loginButton.setOnClickListener {
-            val intent = Intent(this, UserHomeProfile::class.java)
+
+            if(loginUsername.text.trim().isNotEmpty() && loginPassword.text.trim().isNotEmpty()) {
+                val intent = Intent(this, UserHomeProfile::class.java)
                 startActivity(intent)
+            }else
+                Toast.makeText(this, "Email and Password Required",Toast.LENGTH_SHORT).show()
+
         }
 
         registerButton.setOnClickListener {
             val intentSignUp = Intent(this, SignUpPage::class.java)
-                startActivity(intentSignUp)
+            startActivity(intentSignUp)
         }
 
     }
+
+
+
 }
--- app/src/main/java/com/group11/wagapp/SignUpPage.kt
+++ app/src/main/java/com/group11/wagapp/SignUpPage.kt
@@ -3,7 +3,8 @@ package com.group11.wagapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Patterns
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up_page.*
 
 class SignUpPage : AppCompatActivity() {
@@ -12,15 +13,23 @@ class SignUpPage : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up_page)
 
         registerUserButton.setOnClickListener {
-            val intent = Intent(this, FormCollection::class.java)
+
+            if (userSignUpName.text.trim().isNotEmpty() && userEmailAddress.text.trim().isNotEmpty() && signUpPageUsername.text.trim().isNotEmpty()
+                && userPassword.text.trim().isNotEmpty() && userConfirmPassword.text.trim().isNotEmpty()) {
+
+                val intent = Intent(this, FormCollection::class.java)
                 startActivity(intent)
-          }
 
-        userHasAccount.setOnClickListener {
-            val intentLogin = Intent(this, Login::class.java)
+            } else if (userSignUpName.text.trim().isEmpty() && userEmailAddress.text.trim().isEmpty() && signUpPageUsername.text.trim().isEmpty()
+                && userPassword.text.trim().isEmpty() && userConfirmPassword.text.trim().isEmpty()){
+
+                Toast.makeText(this, "All Fields are Required", Toast.LENGTH_SHORT).show()
+            }
+
+            userHasAccount.setOnClickListener {
+                val intentLogin = Intent(this, Login::class.java)
                 startActivity(intentLogin)
+            }
         }
-
     }
-
 }
